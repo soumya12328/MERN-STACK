5974403e5a5426140d47fcb828d0ed2e12d4a039
soumya12328 <soumyasharma9935@gmail.com>
2026-02-24 13:58:10 +0530

    Add lucide-react dependency and implement task deletion in TodoList component

--- Section 2/frontend/src/app/todo-list/page.jsx
+++ Section 2/frontend/src/app/todo-list/page.jsx
@@ -1,14 +1,37 @@
 'use client'
+import { Trash2 } from 'lucide-react';
 import React, { useState } from 'react'
 
 const TodoList = () => {
 
     // let count = 1;
 
-    const [count, setcount] = useState(1)
+    // const [count, setcount] = useState(1)
+
+    const [taskList, settaskList] = useState([]);
+
+    const addNewTask = (e) => {
+        if (e.code === 'Enter') {
+            console.log(e.target.value);
+
+            const newTask = { task: e.target.value, completed: false };
+
+            settaskList([newTask, ...taskList])
+
+            e.target.value = '';
+
+        }
+
+    };
+
+    const deleteTask = (index) => {
+        const temp = taskList;
+        temp.splice(index, 1);
+        settaskList([...temp])
+    }
 
     return (
-        <div className='bg-gray-100 h-screen'>
+        <div className='bg-gray-100 min-h-screen'>
             {/* <button className='border p-4' onClick={() => { alert('button was clicked'); }}>
                 submit
             </button>
@@ -24,19 +47,39 @@ const TodoList = () => {
                 }}
             >add count</button> */}
 
-            <div className='container py-10 mx-auto'> 
+            <div className='container py-10 mx-auto'>
                 <h1 className='text-2xl text-center font-bold'>Todo List</h1>
                 <div className='border rounded-xl bg-white shadow-xl'>
                     <div className='p-5'>
                         <input type="text"
-                        className='py-2 px-5 text-lg w-full rounded-md bg-blue-100'
-                        placeholder='Enter Your Task'
+                            onKeyDown={addNewTask}
+                            className='py-2 px-5 text-lg w-full rounded-md bg-blue-100'
+                            placeholder='Enter Your Task'
                         />
                     </div>
                     <div className='p-5'>
-                        <p className='text-center font-bold text-gray-400'>
-                            😢 No Todos to show Yet</p>
 
+                        {
+                            taskList.length === 0 ? (
+
+                                <p className='text-center font-bold text-gray-400'>
+                                    😢 No Todos to show Yet
+                                </p>
+                            ) : (
+                                taskList.map((obj, index) => {
+                                    return <div key={index}
+                                        className='border border-gray-400 p-4 rounded-lg mb-4 flex justify-between items-center'
+                                    >
+                                        <p className='text-lg'>{obj.task}</p>
+                                        <button
+                                            onClick={() => { deleteTask(index) }}
+                                            className='bg-red-500 text-white p-2 rounded-md'>
+                                            <Trash2 />
+                                        </button>
+                                    </div>
+                                })
+                            )
+                        }
                     </div>
                 </div>
             </div>
